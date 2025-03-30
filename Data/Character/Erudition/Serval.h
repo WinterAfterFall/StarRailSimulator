9e5521b3f4cef4ad81e263fd8f4467c6591611fc
WinterAfterFall <tonnam.jit@gmail.com>
2025-03-30 21:13:25 +0700

    Fix Serval

--- Data/Character/Erudition/Serval.h
+++ Data/Character/Erudition/Serval.h
@@ -29,6 +29,7 @@ namespace Serval{
     
     void Setup(int num ,int E,function<void(Ally *ptr)> LC,function<void(Ally *ptr)> Relic,function<void(Ally *ptr)> Planar){
         Ally_unit[num] = make_unique<Ally>();
+        Ally *ptr = Ally_unit[num].get();
         SetBaseStats(Ally_unit[num]->Sub_Unit_ptr[0].get(),917,653,375);
         SetBasicStats(Ally_unit[num].get(),104,100,100,E,"Lightning","Erudition",num,"Serval","Ally");
 
@@ -48,55 +49,126 @@ namespace Serval{
         LC(Ally_unit[num].get());
         Relic(Ally_unit[num].get());
         Planar(Ally_unit[num].get());
-        Ally_unit[num]->Sub_Unit_ptr[0]->Turn_func = Turn_func;
-        void Turn_func(){
-            auto ally_ptr = dynamic_cast<Sub_Unit*>(ptr);
-            if(ptr->Atv_stats->turn_cnt%3!=1){
-                Basic_Atk(ally_ptr->ptr_to_unit);
-            }else{
-                Skill(ally_ptr->ptr_to_unit);
+        Ally_unit[num]->Sub_Unit_ptr[0]->Turn_func = [ptr, allyPtr = ptr->Sub_Unit_ptr[0].get()]() {
+            if (allyPtr->Atv_stats->turn_cnt % 3 != 1) {
+                Basic_Atk(ptr);
+            } else {
+                Skill(ptr);
             }
-        }
+        };
 
-        Reset_List.push_back(TriggerByYourSelf_Func(PRIORITY_IMMEDIATELY,Ally_unit[num].get(),Reset));
-        Ultimate_List.push_back(TriggerByYourSelf_Func(PRIORITY_ACTTACK,Ally_unit[num].get(),Ult_func));
-        After_turn_List.push_back(TriggerByYourSelf_Func(PRIORITY_BUFF,Ally_unit[num].get(),After_turn));
-        Tune_stats_List.push_back(TriggerByYourSelf_Func(PRIORITY_IMMEDIATELY,Ally_unit[num].get(),Tune_stats));
-        Start_game_List.push_back(TriggerByYourSelf_Func(PRIORITY_ACTTACK,Ally_unit[num].get(),Start_game));
-        Dot_List.push_back(TriggerDot_Func(PRIORITY_BUFF,Ally_unit[num].get(),Dot));
-        When_attack_List.push_back(TriggerByAction_Func(PRIORITY_ACTTACK,Ally_unit[num].get(),When_attack));
-        Enemy_Death_List.push_back(TriggerBySomeAlly_Func(PRIORITY_IMMEDIATELY,Ally_unit[num].get(),Enemy_Death_func));
+        Reset_List.push_back(TriggerByYourSelf_Func(PRIORITY_IMMEDIATELY, [ptr]() {
+            ptr->Sub_Unit_ptr[0]->Stats_type["Ehr"]["None"] += 18;
+            ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["None"] += 18.7;
+
+            // relic
+            ptr->Sub_Unit_ptr[0]->Stats_type["Ehr"]["None"] += 43.2;
+            ptr->Sub_Unit_ptr[0]->Atv_stats->Flat_Speed += 25;
+            ptr->Sub_Unit_ptr[0]->Stats_each_element["Dmg%"]["Lightning"]["None"] += 38.8;
+            ptr->Energy_recharge += 19.4;
+
+            // substats
+            ptr->Sub_Unit_ptr[0]->Stats_type["Ehr"]["None"] += ptr->Sub_effect_hit_rate_use;
+            ptr->Sub_Unit_ptr[0]->Atv_stats->Flat_Speed += ptr->Sub_Speed_use;
+        }));
+
+        Ultimate_List.push_back(TriggerByYourSelf_Func(PRIORITY_ACTTACK, [ptr]() {
+            if (!ultUseCheck(ptr)) return;
+            ActionData data_ = ActionData();
+            data_.Ultimate_set(ptr->Sub_Unit_ptr[0].get(), "Aoe", "Serval Ultimate");
+            data_.Add_Target_Other();
+            data_.Damage_spilt.Main.push_back({194, 0, 0, 20});
+            data_.Damage_spilt.Adjacent.push_back({194, 0, 0, 20});
+            data_.Damage_spilt.Other.push_back({194, 0, 0, 20});
+            data_.actionFunction = [ptr](ActionData &data_){
+                Attack(data_);
+                if (ptr->Eidolon >= 4){
+                    for (int i = 1; i <= Total_enemy; i++) {
+                        if (!Debuff_check(Enemy_unit[i].get(), "Serval_Shock")) {
+                            Enemy_unit[i]->Debuff["Serval_Shock"] = 1;
+                            Enemy_unit[i]->Debuff["Shock_check"]++;
+                            Enemy_unit[i]->Total_debuff++;
+                        }
+                        Apply_debuff(ptr->Sub_Unit_ptr[0].get(), Enemy_unit[i].get());
+                        Extend_Debuff_single_target(Enemy_unit[i].get(), "Serval_Shock", 2);
+                    }
+                }
+            };
+            Action_bar.push(data_);
+            
+            
+            if (!actionBarUse) Deal_damage();
+        }));
+
+        After_turn_List.push_back(TriggerByYourSelf_Func(PRIORITY_BUFF, [ptr]() {
+            if (turn->Unit_Name == "Serval") {
+                if (Buff_end(ptr->Sub_Unit_ptr[0].get(), "Serval_A6")) {
+                    ptr->Sub_Unit_ptr[0]->Buff_check["Serval_A6"] = 0;
+                    ptr->Sub_Unit_ptr[0]->Stats_type["Atk%"]["None"] -= 20;
+                }
+            }
+            if (turn->Side == "Enemy") {
+                Enemy *tempstats = dynamic_cast<Enemy*>(turn->ptr_to_unit);
+                if (tempstats) {
+                    if (Debuff_end(tempstats, "Serval_Shock")) {
+                        tempstats->Debuff["Serval_Shock"] = 0;
+                        tempstats->Debuff["Shock_check"]--;
+                        tempstats->Total_debuff--;
+                    }
+                }
+            }
+        }));
+        Tune_stats_List.push_back(TriggerByYourSelf_Func(PRIORITY_IMMEDIATELY, [ptr]() {
+            Cal_Speed_Needed(ptr, 150);
+            Cal_effect_hit_rate(ptr, 100);
+        }));
+
+        Start_game_List.push_back(TriggerByYourSelf_Func(PRIORITY_ACTTACK, [ptr]() {
+            Increase_energy(ptr, 15);
+            if (ptr->Eidolon >= 6) {
+                ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["None"] += 30;
+            }
+        }));
+
+        Dot_List.push_back(TriggerDot_Func(PRIORITY_BUFF, [ptr](Enemy* target, double Dot_ratio, string Dot_type) {
+            if (Dot_type != "None" && Dot_type != "Lightning") return;
+            if (!Debuff_check(target, "Serval_Shock")) return;
+            ActionData data_ = ActionData();
+            data_.Dot_set(ptr->Sub_Unit_ptr[0].get(), "Single_target", "Serval Shock");
+            data_.Damage_spilt.Main.push_back({114, 0, 0, 0});
+            Cal_Dot_damage(data_, target, Dot_ratio);
+        }));
+
+        When_attack_List.push_back(TriggerByAction_Func(PRIORITY_ACTTACK, [ptr](ActionData &data_) {
+            if (data_.Attacker->Atv_stats->Unit_Name != "Serval") return;
+            ActionData data_temp = ActionData();
+            data_temp.Additional_set(ptr->Sub_Unit_ptr[0].get(), "Single_target", "Serval Additional Damage");
+            for (int i = 1; i <= Total_enemy; i++) {
+                if (Debuff_check(Enemy_unit[i].get(), "Serval_Shock")) {
+                    Cal_Additional_damage(data_temp, Enemy_unit[i].get(), {79, 0, 0, 0});
+                    if (ptr->Eidolon >= 2) {
+                        Increase_energy(ptr, 4);
+                    }
+                }
+            }
+        }));
+
+        Enemy_Death_List.push_back(TriggerBySomeAlly_Func(PRIORITY_IMMEDIATELY, [ptr](Enemy *target, Sub_Unit *Killer) {
+            if (!Buff_check(ptr->Sub_Unit_ptr[0].get(), "Serval_A6")) {
+                ptr->Sub_Unit_ptr[0]->Buff_check["Serval_A6"] = 1;
+                ptr->Sub_Unit_ptr[0]->Stats_type["Atk%"]["None"] += 20;
+            }
+            Extend_Buff_single_target(ptr->Sub_Unit_ptr[0].get(), "Serval_A6", 2);
+        }));
 
 
         
     }
-    void Reset(Ally *ptr){
-        ptr->Sub_Unit_ptr[0]->Stats_type["Ehr"]["None"]+=18;
-        ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["None"]+=18.7;
 
 
-        //relic
-        ptr->Sub_Unit_ptr[0]->Stats_type["Ehr"]["None"]+=43.2;
-        ptr->Sub_Unit_ptr[0]->Atv_stats->Flat_Speed+=25;
-        ptr->Sub_Unit_ptr[0]->Stats_each_element["Dmg%"]["Lightning"]["None"]+=38.8;
-        ptr->Energy_recharge +=19.4;
-
-        //substats
-        ptr->Sub_Unit_ptr[0]->Stats_type["Ehr"]["None"]+=ptr->Sub_effect_hit_rate_use;
-        ptr->Sub_Unit_ptr[0]->Atv_stats->Flat_Speed+=ptr->Sub_Speed_use;
-
-
-        
-    }
-    void Tune_stats(Ally *ptr){
-        Cal_Speed_Needed(ptr,150);
-        Cal_effect_hit_rate(ptr,100);
-
-    }
 
     void Basic_Atk(Ally *ptr){
-        Increase_energy(Ally_unit[ptr->Sub_Unit_ptr[0]->Atv_stats->Unit_num].get(),20);
-        Skill_point(ptr->Sub_Unit_ptr[0].get(),1);
+        
         ActionData data_ = ActionData();
         data_.Basic_Attack_set(ptr->Sub_Unit_ptr[0].get(),"Single_target","Serval BasicAttack");
         data_.Add_Target(chooseEnemyTarget(ptr->Sub_Unit_ptr[0].get()));
@@ -110,12 +182,14 @@ namespace Serval{
         data_.Turn_reset=true;
         data_.Damage_spilt.Main.push_back({110,0,0,10});
         if(ptr->Eidolon>=1)data_.Damage_spilt.Adjacent.push_back({60,0,0,0});
-
+        data_.actionFunction = [ptr](ActionData &data_){
+            Increase_energy(Ally_unit[ptr->Sub_Unit_ptr[0]->Atv_stats->Unit_num].get(),20);
+            Skill_point(ptr->Sub_Unit_ptr[0].get(),1);
+            Attack(data_);
+        };
         Action_bar.push(data_);
     }
     void Skill(Ally *ptr){
-        Increase_energy(Ally_unit[ptr->Sub_Unit_ptr[0]->Atv_stats->Unit_num].get(),30);
-        Skill_point(ptr->Sub_Unit_ptr[0].get(),-1);
         ActionData data_ = ActionData();
         data_.Skill_set(ptr->Sub_Unit_ptr[0].get(),"Blast","Serval Skill");
         data_.Add_Target_Adjacent();
@@ -123,93 +197,29 @@ namespace Serval{
         data_.Damage_spilt.Main.push_back({154,0,0,20});
         data_.Damage_spilt.Adjacent.push_back({66,0,0,10});
 
-
-        Action_bar.push(data_);
-        for(int i=1;i<=Total_enemy;i++){
-            if(!Debuff_check(Enemy_unit[i].get(),"Serval_Shock")){
-                Enemy_unit[i]->Debuff["Serval_Shock"] = 1;
-                Enemy_unit[i]->Debuff["Shock_check"]++;
-                Enemy_unit[i]->Total_debuff++;
-            }
-            Apply_debuff(ptr->Sub_Unit_ptr[0].get(),Enemy_unit[i].get());
-            Extend_Debuff_single_target(Enemy_unit[i].get(),"Serval_Shock",2);
-        }
-    }
-
-    void Ult_func(Ally *ptr){
-        if(!ultUseCheck(ptr))return;
-            ActionData data_ = ActionData();
-            data_.Ultimate_set(ptr->Sub_Unit_ptr[0].get(),"Aoe","Serval Ultimate");
-            data_.Add_Target_Other();
-            data_.Damage_spilt.Main.push_back({194,0,0,20});
-            data_.Damage_spilt.Adjacent.push_back({194,0,0,20});
-            data_.Damage_spilt.Other.push_back({194,0,0,20});
-            Action_bar.push(data_);
-            if(ptr->Eidolon<4)return;
+        data_.actionFunction = [ptr](ActionData &data_){
+            Increase_energy(ptr,30);
+            Skill_point(ptr->Sub_Unit_ptr[0].get(),-1);
             for(int i=1;i<=Total_enemy;i++){
-            if(!Debuff_check(Enemy_unit[i].get(),"Serval_Shock")){
-                Enemy_unit[i]->Debuff["Serval_Shock"] = 1;
-                Enemy_unit[i]->Debuff["Shock_check"]++;
-                Enemy_unit[i]->Total_debuff++;
-            }
-            Apply_debuff(ptr->Sub_Unit_ptr[0].get(),Enemy_unit[i].get());
-            Extend_Debuff_single_target(Enemy_unit[i].get(),"Serval_Shock",2);
-        }
-        Deal_damage();
-            
-        }
-    void Dot(Enemy* target,Ally *ptr,double Dot_ratio,string Dot_type){
-        if(Dot_type!="None"&&Dot_type!="Lightning")return;
-        if(!Debuff_check(target,"Serval_Shock"))return;
-        ActionData data_ = ActionData();
-        data_.Dot_set(ptr->Sub_Unit_ptr[0].get(),"Single_target","Serval Shock");
-        data_.Damage_spilt.Main.push_back({114,0,0,0});
-        Cal_Dot_damage(data_,target,Dot_ratio);
-    }
-    void After_turn(Ally *ptr){
-        if(turn->Unit_Name=="Serval"){
-            if(Buff_end(ptr->Sub_Unit_ptr[0].get(),"Serval_A6")){
-                ptr->Sub_Unit_ptr[0]->Buff_check["Serval_A6"] = 0;
-                ptr->Sub_Unit_ptr[0]->Stats_type["Atk%"]["None"]-=20;
-            }
-        }
-        if(turn->Side=="Enemy"){
-            Enemy *tempstats = dynamic_cast<Enemy*>(turn->ptr_to_unit);
-            if(tempstats){
-                if(Debuff_end(tempstats,"Serval_Shock")){
-                    tempstats->Debuff["Serval_Shock"] = 0;
-                    tempstats->Debuff["Shock_check"]--;
-                    tempstats->Total_debuff--;
+                if(!Debuff_check(Enemy_unit[i].get(),"Serval_Shock")){
+                    Enemy_unit[i]->Debuff["Serval_Shock"] = 1;
+                    Enemy_unit[i]->Debuff["Shock_check"]++;
+                    Enemy_unit[i]->Total_debuff++;
                 }
+                Apply_debuff(ptr->Sub_Unit_ptr[0].get(),Enemy_unit[i].get());
+                Extend_Debuff_single_target(Enemy_unit[i].get(),"Serval_Shock",2);
             }
-        }
+            Attack(data_);
+        };
+        Action_bar.push(data_);
+        
     }
-    void When_attack(Ally *ptr,ActionData &data_){
-        if(data_.Attacker->Atv_stats->Unit_Name!="Serval")return;
-        ActionData data_temp = ActionData();
-        data_temp.Additional_set(ptr->Sub_Unit_ptr[0].get(),"Single_target","Serval Additional Damage");
-        for(int i=1;i<=Total_enemy;i++){
-            if(Debuff_check(Enemy_unit[i].get(),"Serval_Shock")){
-                Cal_Additional_damage(data_temp,Enemy_unit[i].get(),{79,0,0,0});
-                if(ptr->Eidolon>=2){
-                    Increase_energy(ptr,4);
-                }
-            }
-        }
-    }
-    void Enemy_Death_func(Ally *ptr,Enemy *target,Sub_Unit *Killer){
-        if(!Buff_check(ptr->Sub_Unit_ptr[0].get(),"Serval_A6")){
-            ptr->Sub_Unit_ptr[0]->Buff_check["Serval_A6"] = 1;
-            ptr->Sub_Unit_ptr[0]->Stats_type["Atk%"]["None"]+=20;
 
-        }
-        Extend_Buff_single_target(ptr->Sub_Unit_ptr[0].get(),"Serval_A6",2);
-    }
-    void Start_game(Ally *ptr){
-        Increase_energy(ptr,15);
-        if(ptr->Eidolon>=6){
-        ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["None"]+=30;
-        }
-    }
+
+
+
+
+
+
 }
 #endif
