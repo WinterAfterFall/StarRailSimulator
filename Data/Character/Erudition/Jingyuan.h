122efbe4151ded12b05a8714d9f7ba6c8755daf5
WinterAfterFall <tonnam.jit@gmail.com>
2025-03-30 18:34:45 +0700

    Fix Jingyuan

--- Data/Character/Erudition/Jingyuan.h
+++ Data/Character/Erudition/Jingyuan.h
@@ -38,45 +38,12 @@ namespace Jingyuan{
     void Hit_func(Ally *ptr, int Hit_cnt);
     void Ult_func(Ally *ptr);//*
     */
-    void Reset(Ally *ptr){
-        ptr->Sub_Unit_ptr[0]->Stats_type["Atk%"]["None"]+=28;
-        ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["None"]+=12;
-        ptr->Sub_Unit_ptr[0]->Stats_type["Def%"]["None"]+=12.5;
 
-
-        //relic
-        ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["None"]+=32.4;
-        ptr->Sub_Unit_ptr[0]->Atv_stats->Flat_Speed+=25;
-        ptr->Sub_Unit_ptr[0]->Stats_each_element["Dmg%"]["Lightning"]["None"]+=38.8;
-        ptr->Sub_Unit_ptr[0]->Stats_type["Atk%"]["None"]+=43.2;
-
-        //substats
-        ptr->Sub_Unit_ptr[0]->Atv_stats->Flat_Speed+=11.5;
-
-        //LL
-        ptr->Sub_Unit_ptr[0]->Stack["LL_stack"]=3;
-        ptr->Summon_ptr[0]->Atv_stats->Flat_Speed=0;
-        ptr->Summon_ptr[0]->Atv_stats->Speed_percent=0;
-
-        
-    }
     void Setup_Jingyuan(int num ,int E,function<void(Ally *ptr)> LC,function<void(Ally *ptr)> Relic,function<void(Ally *ptr)> Planar){
         Ally_unit[num] = make_unique<Ally>();
-
-        Ally_unit[num]->Sub_Unit_ptr[0]->Base_hp = 1164;
-        Ally_unit[num]->Sub_Unit_ptr[0]->Base_atk = 698;
-        Ally_unit[num]->Sub_Unit_ptr[0]->Base_def = 485;
-        Ally_unit[num]->Sub_Unit_ptr[0]->Atv_stats->Base_speed = 99;
-        Ally_unit[num]->Max_energy = 130;
-        Ally_unit[num]->Ult_cost = 130;
-        Ally_unit[num]->Eidolon = E;
-        Ally_unit[num]->Sub_Unit_ptr[0]->Element_type.push_back("Lightning");
-        Ally_unit[num]->Path.push_back("Erudition");
-        Ally_unit[num]->Sub_Unit_ptr[0]->Atv_stats->Unit_num = num;
-        Ally_unit[num]->Sub_Unit_ptr[0]->Atv_stats->Char_Name = "Jingyuan";
-        Ally_unit[num]->Sub_Unit_ptr[0]->Atv_stats->Unit_Name = "Jingyuan";
-        Ally_unit[num]->Sub_Unit_ptr[0]->Atv_stats->Side = "Ally";
-        Ally_unit[num]->Sub_Unit_ptr[0]->Atv_stats->ptr_to_unit = Ally_unit[num]->Sub_Unit_ptr[0].get();
+        Ally *ptr = Ally_unit[num].get();
+        SetBaseStats(Ally_unit[num]->Sub_Unit_ptr[0].get(), 1164, 698, 485);
+        SetBasicStats(Ally_unit[num].get(), 99, 130, 130, E, "Lightning", "Erudition", num, "Jingyuan", "Ally");
         
 
         //substats
@@ -94,61 +61,129 @@ namespace Jingyuan{
         LC(Ally_unit[num].get());
         Relic(Ally_unit[num].get());
         Planar(Ally_unit[num].get());
-        Ally_unit[num]->Sub_Unit_ptr[0]->Turn_func = Turn_func;
-        Ultimate_List.push_back(TriggerByYourSelf_Func(PRIORITY_ACTTACK,Ally_unit[num].get(),Ult_func));
-        After_turn_List.push_back(TriggerByYourSelf_Func(PRIORITY_IMMEDIATELY,Ally_unit[num].get(),After_turn));
-        Reset_List.push_back(TriggerByYourSelf_Func(PRIORITY_IMMEDIATELY,Ally_unit[num].get(),Reset));
-        Start_game_List.push_back(TriggerByYourSelf_Func(PRIORITY_IMMEDIATELY,Ally_unit[num].get(),Start_game));
+        Ally_unit[num]->Sub_Unit_ptr[0]->Turn_func = [ptr, allyPtr = ptr->Sub_Unit_ptr[0].get()]() {
+            if ((sp <= Sp_Safety && Robin_temp(ptr)) || allyPtr->Atv_stats->turn_cnt == 1 && Sp_status == "Negative") {
+                Basic_Atk(ptr);
+            } else {
+                Skill(ptr);
+            }
+        };
+
+        Ultimate_List.push_back(TriggerByYourSelf_Func(PRIORITY_ACTTACK, [ptr]() {
+            if (((turn->Char_Name != "Jingyuan" || Ult_After_Turn == 1) && Robin_temp(ptr)) || !ultUseCheck(ptr)) return;
+            ActionData data_ = ActionData();
+            
+            data_.Ultimate_set(ptr->Sub_Unit_ptr[0].get(), "Aoe","Jingyuan Ultimate");
+            data_.Add_Target_Other();
+            data_.Damage_spilt.Main.push_back({200, 0, 0, 20});
+            data_.Damage_spilt.Adjacent.push_back({200, 0, 0, 20});
+            data_.Damage_spilt.Other.push_back({200, 0, 0, 20});
+            data_.actionFunction = [ptr](ActionData &data_){
+                Attack(data_);
+                if (ptr->Print)Char_Command::printUltStart("Jingyuan");
+                ptr->Sub_Unit_ptr[0]->Stack["LL_stack"] += 3;
+                if (ptr->Sub_Unit_ptr[0]->Stack["LL_stack"] >= 10) {
+                    ptr->Summon_ptr[0]->Atv_stats->Flat_Speed = 70;
+                    Speed_Buff(ptr->Summon_ptr[0]->Atv_stats.get(), 0, 0);
+                } else {
+                    Speed_Buff(ptr->Summon_ptr[0]->Atv_stats.get(), 0, 30);
+                }
+            };
+            
+            Action_bar.push(data_);
+            if(!actionBarUse)Deal_damage();
+        }));
+        
+
+        After_turn_List.push_back(TriggerByYourSelf_Func(PRIORITY_IMMEDIATELY, [ptr]() {
+            if (!(ptr->Sub_Unit_ptr[0]->Atv_stats->Unit_num == turn->Unit_num && turn->Side == "Ally")) return;
+            if (Buff_end(ptr->Sub_Unit_ptr[0].get(), "War_Marshal")) {
+                ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["None"] -= 10;
+                ptr->Sub_Unit_ptr[0]->Buff_check["War_Marshal"];
+            }
+            if (ptr->Eidolon >= 2 && Buff_end(ptr->Sub_Unit_ptr[0].get(), "Swing_Skies_Squashed")) {
+                ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["Basic_Attack"] -= 20;
+                ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["Skill"] -= 20;
+                ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["Ultimate"] -= 20;
+                ptr->Sub_Unit_ptr[0]->Buff_check["Swing_Skies_Squashed"] = 0;
+            }
+        }));
+
+
+        Reset_List.push_back(TriggerByYourSelf_Func(PRIORITY_IMMEDIATELY, [ptr]() {
+            ptr->Sub_Unit_ptr[0]->Stats_type["Atk%"]["None"] += 28;
+            ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["None"] += 12;
+            ptr->Sub_Unit_ptr[0]->Stats_type["Def%"]["None"] += 12.5;
+
+            // relic
+            ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["None"] += 32.4;
+            ptr->Sub_Unit_ptr[0]->Atv_stats->Flat_Speed += 25;
+            ptr->Sub_Unit_ptr[0]->Stats_each_element["Dmg%"]["Lightning"]["None"] += 38.8;
+            ptr->Sub_Unit_ptr[0]->Stats_type["Atk%"]["None"] += 43.2;
+
+            // substats
+            ptr->Sub_Unit_ptr[0]->Atv_stats->Flat_Speed += 11.5;
+
+            // LL
+            ptr->Sub_Unit_ptr[0]->Stack["LL_stack"] = 3;
+            ptr->Summon_ptr[0]->Atv_stats->Flat_Speed = 0;
+            ptr->Summon_ptr[0]->Atv_stats->Speed_percent = 0;
+        }));
+
+        Start_game_List.push_back(TriggerByYourSelf_Func(PRIORITY_IMMEDIATELY, [ptr]() {
+            if (ptr->Technique == 1) {
+                ptr->Sub_Unit_ptr[0]->Stack["LL_stack"] += 3;
+                Speed_Buff(ptr->Summon_ptr[0]->Atv_stats.get(), 0, 30);
+            }
+            Increase_energy(ptr, 15);
+        }));
 
 
         //LL
-        Ally_unit[num]->Summon_ptr.resize(1);
-        Ally_unit[num]->Summon_ptr[0] =  make_unique<Unit>();
-        Ally_unit[num]->Summon_ptr[0]->Atv_stats->Base_speed = 60;
-        Ally_unit[num]->Summon_ptr[0]->Atv_stats->Unit_num = num;
-        Ally_unit[num]->Summon_ptr[0]->Atv_stats->Char_Name = "LL";
-        Ally_unit[num]->Summon_ptr[0]->Atv_stats->Side = "Summon";
-        Ally_unit[num]->Summon_ptr[0]->Atv_stats->Unit_Name = "Jingyuan";
-        Ally_unit[num]->Summon_ptr[0]->Atv_stats->ptr_to_unit = Ally_unit[num]->Summon_ptr[0].get();
-        Ally_unit[num]->Summon_ptr[0]->Turn_func = [](Unit *ptr){
-            Ally *ally_ptr = Ally_unit[ptr->Atv_stats->Unit_num].get();
-            if(ally_ptr->Sub_Unit_ptr[0]->Stack["LL_stack"]>=6){
-                ally_ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["Summon"]+=25;
-            }
+        SetSummonStats(ptr, 60, "LL");
+        Ally_unit[num]->Summon_ptr[0]->Turn_func = [ptr](){
+            
             ActionData temp = ActionData();
-            temp.Fua_set(ally_ptr->Sub_Unit_ptr[0].get(),"Bounce");
+            temp.Fua_set(ptr->Sub_Unit_ptr[0].get(),"Bounce","LL Attack");
             temp.Add_Target_Adjacent();
             temp.Skill_Type.push_back("Summon");
             temp.Turn_reset = 1;
-            //cout<<"                         LL stack = "<<Ally_unit[1]->stats->Stack["LL_stack"]<<endl;
-            for(int i=1;i<=ally_ptr->Sub_Unit_ptr[0]->Stack["LL_stack"];i++){
-                temp.Damage_spilt.Main.push_back({66,0,0,5});
-                if(ally_ptr->Eidolon>=1){
-                    temp.Damage_spilt.Adjacent.push_back({33,0,0,0});
-                }else{
-                    temp.Damage_spilt.Adjacent.push_back({66*0.25,0,0,0});
+            temp.actionFunction = [ptr](ActionData &data_){
+                if(ptr->Sub_Unit_ptr[0]->Stack["LL_stack"]>=6){
+                    ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["Summon"]+=25;
                 }
-                
-            }
 
-            if(ally_ptr->Sub_Unit_ptr[0]->Stack["LL_stack"]>=6){
-                ally_ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["Summon"]-=25;
-            }
-    
-            turn->Flat_Speed = 0;
-            ally_ptr->Sub_Unit_ptr[0]->Stack["LL_stack"] = 3;
+                for(int i=1;i<=ptr->Sub_Unit_ptr[0]->Stack["LL_stack"];i++){
+                    data_.Damage_spilt.Main.push_back({66,0,0,5});
+                    if(ptr->Eidolon>=1){
+                        data_.Damage_spilt.Adjacent.push_back({33,0,0,0});
+                    }else{
+                        data_.Damage_spilt.Adjacent.push_back({66*0.25,0,0,0});
+                    }
+                    
+                }
+                Attack(data_);
+
+                if(ptr->Sub_Unit_ptr[0]->Stack["LL_stack"]>=6){
+                    ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["Summon"]-=25;
+                }
+        
+                turn->Flat_Speed = 0;
+                ptr->Sub_Unit_ptr[0]->Stack["LL_stack"] = 3;
+                
+                
+                if(ptr->Eidolon>=2){
+                Extend_Buff_single_target(ptr->Sub_Unit_ptr[0].get(),"Swing_Skies_Squashed",2);
+                
+                if(!Buff_check(ptr->Sub_Unit_ptr[0].get(),"Swing_Skies_Squashed")){
+                    ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["Basic_Attack"]+=20;
+                    ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["Skill"]+=20;
+                    ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["Ultimate"]+=20;
+                    ptr->Sub_Unit_ptr[0]->Buff_check["Swing_Skies_Squashed"] = 1;
+                }
+                }
+            };
             
-            
-            if(ally_ptr->Eidolon>=2){
-            Extend_Buff_single_target(ally_ptr->Sub_Unit_ptr[0].get(),"Swing_Skies_Squashed",2);
-            
-            if(!Buff_check(ally_ptr->Sub_Unit_ptr[0].get(),"Swing_Skies_Squashed")){
-                ally_ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["Basic_Attack"]+=20;
-                ally_ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["Skill"]+=20;
-                ally_ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["Ultimate"]+=20;
-                ally_ptr->Sub_Unit_ptr[0]->Buff_check["Swing_Skies_Squashed"] = 1;
-            }
-            }
             Action_bar.push(temp);
             
 
@@ -160,30 +195,26 @@ namespace Jingyuan{
          
             
     }
-    void Turn_func(Unit *ptr){
-        auto ally_ptr = dynamic_cast<Sub_Unit*>(ptr);
-        if((sp<=Sp_Safety && Robin_temp(ally_ptr->ptr_to_unit)||ally_ptr->Atv_stats->turn_cnt==1)&&Sp_status=="Negative"){
-            Basic_Atk(ally_ptr->ptr_to_unit);
-        }else{
-            Skill(ally_ptr->ptr_to_unit);
-        }
-    }
+
     void Basic_Atk(Ally *ptr){
-        Increase_energy(Ally_unit[ptr->Sub_Unit_ptr[0]->Atv_stats->Unit_num].get(),20);
-        Skill_point(ptr->Sub_Unit_ptr[0].get(),1);
+        
         ActionData data_ = ActionData();
-        data_.Basic_Attack_set(ptr->Sub_Unit_ptr[0].get(),"Single_target");
+        data_.Basic_Attack_set(ptr->Sub_Unit_ptr[0].get(),"Single_target","Jingyuan BasicAtttack");
         data_.Add_Target(chooseEnemyTarget(ptr->Sub_Unit_ptr[0].get()));
         data_.Turn_reset=true;
         data_.Damage_spilt.Main.push_back({55,0,0,5.5});
         data_.Damage_spilt.Main.push_back({45,0,0,4.5});
+        data_.actionFunction = [ptr] (ActionData &data_){
+            Increase_energy(ptr,20);
+            Skill_point(ptr->Sub_Unit_ptr[0].get(),1);
+            Attack(data_);
+        };
         Action_bar.push(data_);
     }
     void Skill(Ally *ptr){
-        Increase_energy(Ally_unit[ptr->Sub_Unit_ptr[0]->Atv_stats->Unit_num].get(),30);
-        Skill_point(ptr->Sub_Unit_ptr[0].get(),-1);
+        
         ActionData data_ = ActionData();
-        data_.Skill_set(ptr->Sub_Unit_ptr[0].get(),"Aoe");
+        data_.Skill_set(ptr->Sub_Unit_ptr[0].get(),"Aoe","Jingyuan Skill");
         data_.Add_Target_Other();
         data_.Turn_reset=true;
         data_.Damage_spilt.Main.push_back({40,0,0,4});
@@ -197,61 +228,30 @@ namespace Jingyuan{
         data_.Damage_spilt.Other.push_back({40,0,0,4});
         data_.Damage_spilt.Other.push_back({30,0,0,3});
         data_.Damage_spilt.Other.push_back({30,0,0,3});
-        Action_bar.push(data_);
-        if(!Buff_check(ptr->Sub_Unit_ptr[0].get(),"War_Marshal")){
-            ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["None"]+=10;
-            ptr->Sub_Unit_ptr[0]->Buff_check["War_Marshal"]=1;
-
-        }
-        Extend_Buff_single_target(ptr->Sub_Unit_ptr[0].get(),"War_Marshal",2);
-        ptr->Sub_Unit_ptr[0]->Stack["LL_stack"]+=2;
-        if(ptr->Sub_Unit_ptr[0]->Stack["LL_stack"]>=10){
-            ptr->Summon_ptr[0]->Atv_stats->Flat_Speed=70;
-            Speed_Buff(ptr->Summon_ptr[0]->Atv_stats.get(),0,0);
-        }else{
-            Speed_Buff(ptr->Summon_ptr[0]->Atv_stats.get(),0,20);
-        }
-    }
-
-    void Ult_func(Ally *ptr){
-        if(((turn->Char_Name!="Jingyuan"||Ult_After_Turn==1)&&Robin_temp(ptr)) || !ultUseCheck(ptr))return;
-            ActionData data_ = ActionData();
-            if(ptr->Print)cout<<"--------------------------------------------------- Jingyuan Ult"<<endl;
-            data_.Ultimate_set(ptr->Sub_Unit_ptr[0].get(),"Aoe");
-            data_.Add_Target_Other();
-            data_.Damage_spilt.Main.push_back({200,0,0,20});
-            data_.Damage_spilt.Adjacent.push_back({200,0,0,20});
-            data_.Damage_spilt.Other.push_back({200,0,0,20});
-            ptr->Sub_Unit_ptr[0]->Stack["LL_stack"]+=3;
+        data_.actionFunction = [ptr] (ActionData &data_){
+            Increase_energy(ptr,30);
+            Skill_point(ptr->Sub_Unit_ptr[0].get(),-1);
+            if(!Buff_check(ptr->Sub_Unit_ptr[0].get(),"War_Marshal")){
+                ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["None"]+=10;
+                ptr->Sub_Unit_ptr[0]->Buff_check["War_Marshal"]=1;
+    
+            }
+            Extend_Buff_single_target(ptr->Sub_Unit_ptr[0].get(),"War_Marshal",2);
+            ptr->Sub_Unit_ptr[0]->Stack["LL_stack"]+=2;
             if(ptr->Sub_Unit_ptr[0]->Stack["LL_stack"]>=10){
-            ptr->Summon_ptr[0]->Atv_stats->Flat_Speed=70;
-            Speed_Buff(ptr->Summon_ptr[0]->Atv_stats.get(),0,0);
-        }else{
-            Speed_Buff(ptr->Summon_ptr[0]->Atv_stats.get(),0,30);
-        }
-            Action_bar.push(data_);
-            Deal_damage();
+                ptr->Summon_ptr[0]->Atv_stats->Flat_Speed=70;
+                Speed_Buff(ptr->Summon_ptr[0]->Atv_stats.get(),0,0);
+            }else{
+                Speed_Buff(ptr->Summon_ptr[0]->Atv_stats.get(),0,20);
+            }
+            Attack(data_);
+        };
+        Action_bar.push(data_);
+        
     }
-    void After_turn(Ally *ptr){
-        if(!(ptr->Sub_Unit_ptr[0]->Atv_stats->Unit_num == turn->Unit_num && turn->Side=="Ally"))return;
-        if(Buff_end(ptr->Sub_Unit_ptr[0].get(),"War_Marshal")){
-            ptr->Sub_Unit_ptr[0]->Stats_type["Crit_rate"]["None"]-=10;
-            ptr->Sub_Unit_ptr[0]->Buff_check["War_Marshal"];
-        }
-        if(ptr->Eidolon>=2&&Buff_end(ptr->Sub_Unit_ptr[0].get(),"Swing_Skies_Squashed")){
-                ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["Basic_Attack"]-=20;
-                ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["Skill"]-=20;
-                ptr->Sub_Unit_ptr[0]->Stats_type["Dmg%"]["Ultimate"]-=20;
-                ptr->Sub_Unit_ptr[0]->Buff_check["Swing_Skies_Squashed"] = 0;
-        }
-    }
-    void Start_game(Ally *ptr){
-        if(ptr->Technique==1){
-            ptr->Sub_Unit_ptr[0]->Stack["LL_stack"]+=3;
-            Speed_Buff(ptr->Summon_ptr[0]->Atv_stats.get(),0,30);
-        }
-        Increase_energy(ptr,15);
-    }
+
+
+
     bool Temp_Turn_Condition(Unit *ptr){
         return true;
     }
