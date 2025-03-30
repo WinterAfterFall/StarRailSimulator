868554b522a11a71327b2aef4e1054534d6f0a8d
WinterAfterFall <tonnam.jit@gmail.com>
2025-03-31 00:11:32 +0700

    Edit all abundance LC

--- Data/Lightcone/Abundance/Multiplication.h
+++ Data/Lightcone/Abundance/Multiplication.h
@@ -13,20 +13,19 @@ namespace Abundance_Lightcone{
         ptr->Sub_Unit_ptr[0]->Base_atk += 318;
         ptr->Sub_Unit_ptr[0]->Base_def += 198;
         ptr->Light_cone.Name = "Multiplication";
-        After_attack_List.push_back(TriggerByAction_Func(PRIORITY_IMMEDIATELY,ptr,[](Ally *ptr, ActionData &data_){
-            
-                if(data_.Attacker->Atv_stats->Unit_Name == ptr->Sub_Unit_ptr[0]->Atv_stats->Unit_Name&& data_.Action_type.second=="Basic_Attack"){
-                    Action_forward(turn,20);
-                }
-            
+        After_attack_List.push_back(TriggerByAction_Func(PRIORITY_IMMEDIATELY, [ptr](ActionData &data_){
+                    
+            if(data_.Attacker->Atv_stats->Unit_Name == ptr->Sub_Unit_ptr[0]->Atv_stats->Unit_Name && data_.Action_type.second == "Basic_Attack"){
+                Action_forward(turn, 20);
             }
-        ));
-        Buff_List.push_back(TriggerByAction_Func(PRIORITY_IMMEDIATELY,ptr,[](Ally *ptr, ActionData &data_){
-            
-            if(data_.Attacker->Atv_stats->Unit_Name == ptr->Sub_Unit_ptr[0]->Atv_stats->Unit_Name&& data_.Action_type.second=="Basic_Attack"){
-                Action_forward(turn,20);
+        
+        }));
+        Buff_List.push_back(TriggerByAction_Func(PRIORITY_IMMEDIATELY, [ptr](ActionData &data_){
+
+            if(data_.Attacker->Atv_stats->Unit_Name == ptr->Sub_Unit_ptr[0]->Atv_stats->Unit_Name && data_.Action_type.second == "Basic_Attack"){
+                Action_forward(turn, 20);
             }
-            
+
             }
         ));
             
