669949574db96b631761623875524eb53ad0d270
WinterAfterFall <tonnam.jit@gmail.com>
2025-08-10 13:42:16 +0700

    feat: LC Sampo

---
+++ src/Defination/Data/Lightcone/Nihility/Eyes of the Prey.h
@@ -0,0 +1,14 @@
+#include "../include.h"
+namespace Nihility_Lightcone{
+    function<void(Ally *ptr)> EyesOfThePrey(int superimpose){
+        return [=](Ally *ptr) {
+            ptr->SetAllyBaseStats(953,476,331);
+            ptr->Light_cone.Name = "Eyes of the Prey";
+    
+            Reset_List.push_back(TriggerByYourSelf_Func(PRIORITY_IMMEDIATELY, [ptr,superimpose]() {
+                ptr->Sub_Unit_ptr[0]->Stats_type[Stats::EHR][AType::None] += 15 + superimpose * 5;
+                ptr->Sub_Unit_ptr[0]->Stats_type[Stats::DMG][AType::Dot] += 18 + superimpose * 6;
+            }));
+        };
+    }
+}
